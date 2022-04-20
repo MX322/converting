e515ba038e9fa4ebb15a1e5a90a95995a89439a1
Александр Данильченко <mlemix32@gmail.com>
2022-04-20 23:57:10 +0300

    Добавьте файлы проекта.

---
+++ convert.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	double hrn;
+
+	cout << "Enter hrn : ";
+	cin >> hrn;
+
+	double usd = hrn / 29;
+	double eur = hrn / 32;
+
+	cout << "UAH to USD : " << usd << "\n";
+	cout << "EUR to USD : " << eur << "\n";
+
+	return 0;
+}
